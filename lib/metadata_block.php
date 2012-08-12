ab27960e30a5c9254f83b725eea8f5450134c00b
topac <topac@users.noreply.github.com>
2012-08-12 21:31:29 +0200

    style fix

--- lib/metadata_block.php
+++ lib/metadata_block.php
@@ -2,15 +2,14 @@
   namespace SHOUTcastRipper;
 
   class MetadataBlock {
-    private $expected_length;
-    private $content;
+    private $expected_length, $content;
 
-    public function __construct($expected_length){
+    public function __construct($expected_length) {
       $this->content = '';
       $this->expected_length = $expected_length;
     }
 
-    public function expected_length(){
+    public function expected_length() {
       return $this->expected_length;
     }
 
@@ -18,23 +17,23 @@
       $this->content .= $data;
     }
 
-    public function content(){
+    public function content() {
       return $this->content;
     }
 
-    public function length(){
+    public function length() {
       return strlen($this->content);
     }
 
-    public function is_complete(){
+    public function is_complete() {
       return $this->remaining_length() == 0;
     }
 
-    public function remaining_length(){
+    public function remaining_length() {
       return $this->expected_length - $this->length();
     }
 
-    public function stream_title(){
+    public function stream_title() {
       if (!$this->is_complete()) return null;
       $start = strlen("StreamTitle=");
       $end = strpos($this->content, ";", $start);
