eb07be3436872e66025252916d405c4d155125d9
topac <topac@users.noreply.github.com>
2012-08-15 15:03:41 +0200

    adds comments

--- lib/reponse_header.php
+++ lib/reponse_header.php
@@ -9,6 +9,9 @@
       $this->content .= $buffer;
     }
 
+    /**
+     * Gets the position of the last line of the http header. The last line is "\r\n\r\n".
+     */
     private function empty_line_index(){
       if ($this->empty_line_index != null)
         return $this->empty_line_index;
@@ -16,10 +19,17 @@
       return $index ? $index+4 : null;
     }
 
+    /**
+     * Returns true if all the http header has been writed.
+     */
     public function is_complete(){
       return !!$this->empty_line_index();
     }
 
+    /**
+     * Removes from $content all the bytes after the end of the http header,
+     * and returns those bytes.
+     */
     public function remove_tail_audio_data() {
       if (!$this->is_complete()) throw new \Exception("headers not completed");
       $headers = substr($this->content, 0, $this->empty_line_index());
@@ -32,6 +42,11 @@
       return strlen($this->content);
     }
 
+    /**
+     * Returns true if there are any bytes after the http header.
+     * If true, those bytes are the audio data and they can be getted
+     * using the remove_tail_audio_data() method.
+     */
     public function contains_audio_data(){
       return $this->is_complete() && $this->empty_line_index()+4 < $this->length();
     }
