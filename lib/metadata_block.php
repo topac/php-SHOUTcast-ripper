74b5b6b4096b3354b0ae7b8406871685bcf4be9e
topac <topac@users.noreply.github.com>
2012-08-15 14:52:54 +0200

    adds comments

--- lib/metadata_block.php
+++ lib/metadata_block.php
@@ -4,6 +4,10 @@
   class MetadataBlock {
     private $expected_length, $content;
 
+    /**
+     * Creates a new empty MetadataBlock with the given $expected_length.
+     * The actual content can be added using the write() method.
+     */
     public function __construct($expected_length) {
       $this->content = '';
       $this->expected_length = $expected_length;
@@ -25,6 +29,10 @@
       return strlen($this->content);
     }
 
+    /**
+     * Determines if the metadata block is complete. A metadata block is completed
+     * when its content has at least $expected_length bytes of data.
+     */
     public function is_complete() {
       return $this->remaining_length() == 0;
     }
@@ -33,6 +41,11 @@
       return $this->expected_length - $this->length();
     }
 
+    /**
+     * Gets the title of the song/track.
+     * For example, if the metadata is "StreamTitle='Ari Shine - Crank It Out!';StreamUrl='http://digitalia.fm'"
+     * this method will return the string "Ari Shine - Crank It Out!".
+     */
     public function stream_title() {
       if (!$this->is_complete())
         throw new \Exception("The metadata block is not complete yet");
